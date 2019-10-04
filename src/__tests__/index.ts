0e3f4a33c90db4ac4e641da42218dbf35810f82e
Dyaa Eldin <mail@dyaa.me>
2019-10-04 22:31:44 +0200

    impr: Rewrite in typescript

---
+++ src/__tests__/index.ts
@@ -0,0 +1,56 @@
+import sslChecker from "../";
+
+const validSslHost = "badssl.com";
+const expiredSSlHost = "expired.badssl.com";
+const wrongHostDomain = "wrong.host.badssl.com";
+
+describe("sslChecker", () => {
+  it("Should return valid values when valid host is passed", async () => {
+    const sslDetails = await sslChecker(validSslHost);
+
+    expect(sslDetails).toEqual(
+      expect.objectContaining({
+        daysRemaining: expect.any(Number),
+        valid: true,
+        validFrom: expect.any(String),
+        validTo: expect.any(String)
+      })
+    );
+  });
+
+  it("Should return valid = false when provided an expired domain", async () => {
+    const sslDetails = await sslChecker(expiredSSlHost);
+
+    expect(sslDetails).toEqual(
+      expect.objectContaining({
+        valid: false
+      })
+    );
+  });
+
+  it("Should return an error when passing a wrong host domain", async () => {
+    try {
+      await sslChecker(wrongHostDomain);
+    } catch (e) {
+      expect(e).toContain(
+        "getaddrinfo ENOTFOUND expiredSSlHost expiredSSlHost:"
+      );
+    }
+  });
+
+  it("Should return 'Invalid host' when no host provided", async () => {
+    try {
+      await sslChecker();
+    } catch (e) {
+      expect(e).toEqual(new Error("Invalid host"));
+    }
+  });
+
+  it("Should return 'Invalid port' when no port provided", async () => {
+    try {
+      await sslChecker(validSslHost, { port: "port" });
+    } catch (e) {
+      expect(e).toEqual(new Error("Invalid port"));
+    }
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import * as http from "http";
+import * as https from "https";
+import * as tls from "tls";
+
+interface IResolvedValues {
+  valid: boolean;
+  validFrom: string;
+  validTo: string;
+  daysRemaining: number;
+}
+
+const checkPort = (port: unknown): boolean =>
+  !isNaN(parseFloat(port as string)) && Math.sign(port as number) === 1;
+const getDaysBetween = (validFrom: Date, validTo: Date): number =>
+  Math.round(Math.abs(+validFrom - +validTo) / 8.64e7);
+const getDaysRemaining = (validFrom: Date, validTo: Date): number => {
+  const daysRemaining = getDaysBetween(validFrom, validTo);
+
+  if (new Date(validTo).getTime() < new Date().getTime()) {
+    return -daysRemaining;
+  }
+
+  return daysRemaining;
+};
+
+const sslChecker = (
+  host: string,
+  options: Partial<https.RequestOptions> = {
+    agent: false,
+    method: "HEAD",
+    port: 443,
+    rejectUnauthorized: false
+  }
+): Promise<IResolvedValues> =>
+  new Promise((resolve, reject) => {
+    const isValidHostName =
+      host &&
+      /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/.test(
+        host
+      );
+
+    if (!isValidHostName) {
+      reject(new Error("Invalid host"));
+    }
+
+    if (!checkPort(options.port)) {
+      reject(Error("Invalid port"));
+    }
+
+    try {
+      const req = https.request(
+        { host, ...options },
+        (res: http.IncomingMessage) => {
+          const {
+            valid_from,
+            valid_to
+          } = (res.connection as tls.TLSSocket).getPeerCertificate();
+
+          const validFrom = new Date(valid_from);
+          const validTo = new Date(valid_to);
+
+          resolve({
+            daysRemaining: getDaysRemaining(validFrom, validTo),
+            valid:
+              ((res.socket as { authorized?: boolean })
+                .authorized as boolean) || false,
+            validFrom: validFrom.toISOString(),
+            validTo: validTo.toISOString()
+          });
+        }
+      );
+
+      req.on("error", reject);
+      req.end();
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+module.exports = sslChecker;
